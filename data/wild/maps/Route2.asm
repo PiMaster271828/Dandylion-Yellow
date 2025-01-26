5175859f159f1fea460543b40675b10633462d48
G-Dubs <wie15003@byui.edu>
2025-01-25 19:13:25 -0500

    Changed the wild encounters for the first routes

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -1,16 +1,23 @@
 Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, RATTATA
-	db  3, PIDGEY
-	db  4, RATTATA
-	db  4, NIDORAN_M
-	db  4, NIDORAN_F
-	db  5, PIDGEY
-	db  6, NIDORAN_M
-	db  6, NIDORAN_F
-	db  7, PIDGEY
-	db  7, PIDGEY
+	db  3, RATTATA        ; 20%
+	db  3, PIDGEY         ; 20%
+	db  4, RATTATA        ; 15%
+	db  4, NIDORAN_M      ; 10%
+	db  4, NIDORAN_F      ; 10%
+	db  5, PIDGEY         ; 10%
+	db  6, NIDORAN_M      ;  5%
+	db  6, NIDORAN_F      ;  5%
+	db  4, CATERPIE       ;  4%
+	db  4, WEEDLE         ;  1%
 	end_grass_wildmons
+	
+	; Rattata   35%
+	; Pidgey    30%
+    ; Nidoran♂  15%
+	; Nidoran♀  15%
+	; Caterpie   4%
+	; Weedle     1%
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
