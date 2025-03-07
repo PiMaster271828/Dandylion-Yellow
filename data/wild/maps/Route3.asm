3c28f673f91c6b6ed888b5364bc3cb0eb3b9b42d
G-Dubs <wie15003@byui.edu>
2025-03-07 05:58:22 -0500

    Route 3 & 4 Wild Encounter Tables

--- data/wild/maps/Route3.asm
+++ data/wild/maps/Route3.asm
@@ -1,16 +1,23 @@
 Route3WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db  8, SPEAROW
-	db  9, SPEAROW
-	db  9, MANKEY
-	db 10, SPEAROW
-	db  8, SANDSHREW
-	db 10, RATTATA
-	db 10, SANDSHREW
-	db 12, RATTATA
-	db 11, SPEAROW
-	db 12, SPEAROW
+	db  8, PIDGEY         ; 20%  
+	db  9, SPEAROW        ; 20%
+	db 10, RATTATA        ; 15%
+	db  9, MANKEY         ; 10%
+	db 10, SANDSHREW      ; 10%
+	db 10, SPEAROW        ; 10%
+	db  7, JIGGLYPUFF     ;  5%
+	db 12, RATTATA        ;  5%
+	db 11, SPEAROW        ;  4%
+	db 12, SPEAROW        ;  1%
 	end_grass_wildmons
+
+    ; Spearow     ; 40%
+    ; Pidgey      ; 20%
+    ; Rattata     ; 20%
+    ; Mankey      ; 10%
+    ; Sandshrew   ; 10%
+    ; Jigglypuff  ;  5%
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -1,16 +1,23 @@
 Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db  8, SPEAROW
-	db  9, SPEAROW
-	db  9, MANKEY
-	db 10, SPEAROW
-	db  8, SANDSHREW
-	db 10, RATTATA
-	db 10, SANDSHREW
-	db 12, RATTATA
-	db 11, SPEAROW
-	db 12, SPEAROW
+	db 11, SPEAROW        ; 20%
+	db 13, SPEAROW        ; 20%
+	db 10, MANKEY         ; 15%
+	db 12, RATTATA        ; 10%
+	db 10, SANDSHREW      ; 10%
+	db 13, PIDGEY         ; 10%
+	db 12, SANDSHREW      ;  5%
+	db 14, RATTATA        ;  5%
+	db 10, EKANS          ;  4%
+	db 12, EKANS          ;  1%
 	end_grass_wildmons
+
+    ; Spearow    ; 40%
+	; Rattata    ; 15%
+	; Sandshrew  ; 15%
+	; Mankey     ; 15%
+	; Pidgey     ; 10%
+	; Ekans      ;  5%
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
