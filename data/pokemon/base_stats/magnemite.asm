0b8fa781d3d9c4013881a08a59f744f19d8845f6
G-Dubs <wie15003@byui.edu>
2025-03-07 03:33:58 -0500

    Magnemite Line

--- data/pokemon/base_stats/magnemite.asm
+++ data/pokemon/base_stats/magnemite.asm
@@ -1,23 +1,24 @@
 	db DEX_MAGNEMITE ; pokedex id
 
-	db  25,  35,  70,  45,  95
-	;   hp  atk  def  spd  spc
+	db  30,  35,  70,  45,  95 ; 370
+	;   hp  atk  def  spd  spc   tot
 
-	db ELECTRIC, ELECTRIC ; type
+	db ELECTRIC, STEEL ; type
 	db 190 ; catch rate
 	db 89 ; base exp
 
 	INCBIN "gfx/pokemon/front/magnemite.pic", 0, 1 ; sprite dimensions
 	dw MagnemitePicFront, MagnemitePicBack
 
-	db TACKLE, NO_MOVE, NO_MOVE, NO_MOVE ; level 1 learnset
+	db TACKLE, THUNDERSHOCK, NO_MOVE, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
-	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  RAGE,         THUNDERBOLT,  \
-	     THUNDER,      TELEPORT,     MIMIC,        DOUBLE_TEAM,  REFLECT,      \
-	     BIDE,         SWIFT,        REST,         THUNDER_WAVE, SUBSTITUTE,   \
-	     FLASH
+	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,   RAGE,         THUNDERBOLT,  \
+	     THUNDER,      TELEPORT,     MIMIC,         DOUBLE_TEAM,  REFLECT,      \
+	     BIDE,         SELFDESTRUCT, SWIFT,      /*LIGHT_SCREEN,*/REST,         \
+		 THUNDER_WAVE, EXPLOSION,    SUBSTITUTE,    WILD_CHARGE,  MIRROR_COAT,  \
+	   /*FLASH_CANNON, SHOCK_WAVE,*/ FLASH
 	; end
 
 	db 0 ; padding
--- data/pokemon/base_stats/magneton.asm
+++ data/pokemon/base_stats/magneton.asm
@@ -1,23 +1,24 @@
 	db DEX_MAGNETON ; pokedex id
 
-	db  50,  60,  95,  70, 120
-	;   hp  atk  def  spd  spc
+	db  50,  60,  95,  70, 120 ; 515
+	;   hp  atk  def  spd  spc   tot
 
-	db ELECTRIC, ELECTRIC ; type
+	db ELECTRIC, STEEL ; type
 	db 60 ; catch rate
-	db 161 ; base exp
+	db 161 ; base exps
 
 	INCBIN "gfx/pokemon/front/magneton.pic", 0, 1 ; sprite dimensions
 	dw MagnetonPicFront, MagnetonPicBack
 
-	db TACKLE, SONICBOOM, THUNDERSHOCK, NO_MOVE ; level 1 learnset
+	db TACKLE, THUNDERSHOCK, SONICBOOM, THUNDER_WAVE ; level 1 learnset
 	db GROWTH_MEDIUM_FAST ; growth rate
 
 	; tm/hm learnset
-	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  HYPER_BEAM,   RAGE,         \
-	     THUNDERBOLT,  THUNDER,      TELEPORT,     MIMIC,        DOUBLE_TEAM,  \
-	     REFLECT,      BIDE,         SWIFT,        REST,         THUNDER_WAVE, \
-	     SUBSTITUTE,   FLASH
+	tmhm TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,   RAGE,         THUNDERBOLT,  \
+	     THUNDER,      TELEPORT,     MIMIC,         DOUBLE_TEAM,  REFLECT,      \
+	     BIDE,         SELFDESTRUCT, SWIFT,      /*LIGHT_SCREEN,*/REST,         \
+		 THUNDER_WAVE, EXPLOSION,    TRI_ATTACK,    SUBSTITUTE, /*GIGA_IMPACT,*/\
+	   /*WILD_CHARGE,  MIRROR_COAT,  FLASH_CANNON,  SHOCK_WAVE, */FLASH
 	; end
 
 	db 0 ; padding
