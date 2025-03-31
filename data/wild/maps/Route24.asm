3e430f8c0e27cf46b319db014f6aca3be7a8905f
G-Dubs <wie15003@byui.edu>
2025-03-31 01:39:15 -0400

    Changed encounter tables for Routes 24 & 25

--- data/wild/maps/Route24.asm
+++ data/wild/maps/Route24.asm
@@ -1,16 +1,44 @@
 Route24WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 12, ODDISH
-	db 12, BELLSPROUT
-	db 13, PIDGEY
-	db 14, ODDISH
-	db 14, BELLSPROUT
-	db 15, PIDGEY
-	db 13, VENONAT
-	db 16, VENONAT
-	db 17, PIDGEY
-	db 17, PIDGEOTTO
+	db 15, ODDISH         ; 20%
+	db 15, BELLSPROUT     ; 20%
+	db 17, PIDGEY         ; 15%
+	db 10, ABRA           ; 10%
+	db 16, VENONAT        ; 10%
+	db  7, CATERPIE       ; 10% 
+	db  8, METAPOD        ;  5%
+	db  7, WEEDLE         ;  5%
+	db  8, KAKUNA         ;  4%
+	db 17, PIDGEOTTO      ;  1%
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+    ; Oddish      20%
+	; Bellsprout  20%
+	; Pidgey      15%
+	; Abra        10%
+	; Venonat     10%
+	; Caterpie    10%
+	; Metapod      5%
+	; Weedle       5%
+	; Kakuna       4%
+	; Pidgeotto    1%
+
+	def_water_wildmons 3  ; encounter rate
+
+    db 25, GOLDEEN        ; 20%
+	db 30, GOLDEEN        ; 20%
+	db 35, GOLDEEN        ; 15%
+	db 30, PSYDUCK        ; 10%
+	db 30, KRABBY         ; 10%
+	db 30, POLIWAG        ; 10% 
+	db 40, GOLDEEN        ;  5%
+	db 30, SEAKING        ;  5%
+	db 35, SEAKING        ;  4%
+	db 40, SEAKING        ;  1%
 	end_water_wildmons
+	
+	; Goldeen   60%
+	; Psyduck   10%
+	; Krabby    10%
+	; Poliwag   10%
+	; Seaking   10%
--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,16 +1,48 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 12, ODDISH
-	db 12, BELLSPROUT
-	db 13, PIDGEY
-	db 14, ODDISH
-	db 14, BELLSPROUT
-	db 15, PIDGEY
-	db 13, VENONAT
-	db 16, VENONAT
-	db 17, PIDGEY
-	db 17, PIDGEOTTO
+	db 15, ODDISH         ; 20%
+	db 15, BELLSPROUT     ; 20%
+	db 17, PIDGEY         ; 15%
+	db 10, PSYDUCK        ; 10%
+	db 16, VENONAT        ; 10%
+	db  7, CATERPIE       ; 10% 
+	db  8, METAPOD        ;  5%
+	db  7, WEEDLE         ;  5%
+	db  8, KAKUNA         ;  4%
+	db 17, PIDGEOTTO      ;  1%
 	end_grass_wildmons
 
-	def_water_wildmons 0 ; encounter rate
+    ; Oddish      20%
+	; Bellsprout  20%
+	; Pidgey      15%
+	; Psyduck     10%
+	; Venonat     10%
+	; Caterpie    10%
+	; Metapod      5%
+	; Weedle       5%
+	; Kakuna       4%
+	; Pidgeotto    1%
+
+	def_water_wildmons 3  ; encounter rate
+
+    db 30, KRABBY         ; 20%
+	db 35, KRABBY         ; 20%
+	db 30, GOLDEEN        ; 15%
+	db 30, PSYDUCK        ; 10%
+	db 30, SHELLDER       ; 10%
+	db 30, POLIWAG        ; 10% 
+	db 40, KRABBY         ;  5%
+	db 30, SLOWPOKE       ;  5%
+	db 35, KINGLER        ;  4%
+	db 40, KINGLER        ;  1%
 	end_water_wildmons
+	
+	; Krabby    45%
+	; Goldeen   15%
+	; Psyduck   10%
+	; Shellder  10%
+	; Poliwag   10%
+	; Slowpoke   5%
+	; Kingler    5%
+
+    
