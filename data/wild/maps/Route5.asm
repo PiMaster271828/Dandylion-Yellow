d1b9ccd1ea95fff8b225c0f3b2f2811743403944
G-Dubs <wie15003@byui.edu>
2025-03-30 20:05:20 -0400

    Edited Route  wild encounter table

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -1,16 +1,24 @@
 Route5WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 15, PIDGEY
-	db 14, RATTATA
-	db  7, ABRA
-	db 16, PIDGEY
-	db 16, RATTATA
-	db 17, PIDGEY
-	db 17, PIDGEOTTO
-	db  3, JIGGLYPUFF
-	db  5, JIGGLYPUFF
-	db  7, JIGGLYPUFF
+	db 16, PIDGEY         ; 20%
+	db 16, RATTATA        ; 20% 
+	db 10, ABRA           ; 15%
+	db 15, MANKEY         ; 10%
+	db 15, MEOWTH         ; 10%
+	db 18, PIDGEY         ; 10%
+	db 18, RATTATA        ;  5%
+	db  9, JIGGLYPUFF     ;  5%
+	db 11, JIGGLYPUFF     ;  4%
+	db 19, PIDGEOTTO      ;  1%
 	end_grass_wildmons
+
+    ; Pidgey      30%
+	; Rattata     25%
+	; Abra        15%
+	; Mankey      10%
+	; Meowth      10%
+	; Jigglypuff   9%
+	; Pidgeotto    1%
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
