3f7b6c4ca49cb7562fd5e397c6ba30e6d70d8b20
G-Dubs <wie15003@byui.edu>
2025-04-23 14:12:40 -0400

    Added trades from Red & Blue (in comments)

--- data/events/trades.asm
+++ data/events/trades.asm
@@ -6,14 +6,29 @@ TradeMons:
 	; from the Japanese Blue trades, which used species that evolve.
 	; Japanese Red and Green used TRADE_DIALOGSET_CASUAL, and had
 	; the same species as English Red and Blue.
-	db LICKITUNG,  DUGTRIO,  TRADE_DIALOGSET_CASUAL,    "GURIO@@@@@@"
-	db CLEFAIRY,   MR_MIME,  TRADE_DIALOGSET_CASUAL,    "MILES@@@@@@"
-	db BUTTERFREE, BEEDRILL, TRADE_DIALOGSET_HAPPY,     "STINGER@@@@" ; unused
-	db KANGASKHAN, MUK,      TRADE_DIALOGSET_CASUAL,    "STICKY@@@@@"
-	db MEW,        MEW,      TRADE_DIALOGSET_HAPPY,     "BART@@@@@@@" ; unused
-	db TANGELA,    PARASECT, TRADE_DIALOGSET_CASUAL,    "SPIKE@@@@@@"
-	db PIDGEOT,    PIDGEOT,  TRADE_DIALOGSET_EVOLUTION, "MARTY@@@@@@" ; unused
-	db GOLDUCK,    RHYDON,   TRADE_DIALOGSET_EVOLUTION, "BUFFY@@@@@@"
-	db GROWLITHE,  DEWGONG,  TRADE_DIALOGSET_HAPPY,     "CEZANNE@@@@"
-	db CUBONE,     MACHOKE,  TRADE_DIALOGSET_HAPPY,     "RICKY@@@@@@"
+  ; db NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "TERRY@@@@@@" ; Red & Blue Trade
+	db LICKITUNG,  DUGTRIO,   TRADE_DIALOGSET_CASUAL,    "GURIO@@@@@@"
+  ; db NIDORINA,   NIDORINO,  TRADE_DIALOGSET_CASUAL,    "DINO@@@@@@@"
+  ;	db ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@" ; Red & Blue Trade
+	db CLEFAIRY,   MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MILES@@@@@@"
+  ; db ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@" ; Red & Blue Trade	
+	db BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "STINGER@@@@" ; unused
+  ; db POLIWHIRL,  JYNX,      TRADE_DIALOGSET_CASUAL,    "LOLA@@@@@@@" ; Red & Blue Trade
+	db KANGASKHAN, MUK,       TRADE_DIALOGSET_CASUAL,    "STICKY@@@@@"
+  ; db VENONAT,    TANGELA,   TRADE_DIALOGSET_CASUAL,    "CRINKLES@@@" ; Red & Blue Trade
+	db MEW,        MEW,       TRADE_DIALOGSET_HAPPY,     "BART@@@@@@@" ; unused
+  ; db PINSIR,     SCYTHER,   TRADE_DIALOGSET_HAPPY,     "MANTIS@@@@@" ; Trade added by G-Dubs
+	db TANGELA,    PARASECT,  TRADE_DIALOGSET_CASUAL,    "SPIKE@@@@@@"
+  ; db SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "MARC@@@@@@@" ; Red & Blue Trade	
+	db PIDGEOT,    PIDGEOT,   TRADE_DIALOGSET_EVOLUTION, "MARTY@@@@@@" ; unused
+  ; db SPEAROW,    FARFETCHD, TRADE_DIALOGSET_EVOLUTION, "DUX@@@@@@@@" ; Red & Blue Trade
+	db GOLDUCK,    RHYDON,    TRADE_DIALOGSET_EVOLUTION, "BUFFY@@@@@@"
+  ; db PONYTA,     SEEL,      TRADE_DIALOGSET_EVOLUTION, "SAILOR@@@@@" ; Red & Blue Trade	
+	db GROWLITHE,  DEWGONG,   TRADE_DIALOGSET_HAPPY,     "CEZANNE@@@@"
+  ; db ELECTRODE,  RAICHU,    TRADE_DIALOGSET_HAPPY,     "DORIS@@@@@@" ; Red & Blue Trade
+  ; db NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "RITA@@@@@@@" ; Red & Blue Trade
+	db CUBONE,     MACHOKE,   TRADE_DIALOGSET_HAPPY,     "RICKY@@@@@@" 
+  ; db NIDORAN_F,  NIDORAN_M, TRADE_DIALOGSET_HAPPY,     "SPOT@@@@@@@" ; Red & Blue Trade
+  ; db SCYTHER,    PINSIR,    TRADE_DIALOGSET_HAPPY,     "BEETLE@@@@@" ; Trade added by G-Dubs	
+
 	assert_table_length NUM_NPC_TRADES
