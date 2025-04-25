5db1a80ea8ee152296a8f99ad0f2d3e7755c2b80
PiMaster271828 <141529114+PiMaster271828@users.noreply.github.com>
2025-04-25 17:42:54 -0600

    Implemented the Jump Table for alternate sprites

--- home/trainers2.asm
+++ home/trainers2.asm
@@ -22,24 +22,9 @@ GetTrainerInformation::
 	inc de
 	ld a, [hli]
 	ld [de], a
-;	call IsFightingJessieJames
-	farcall IsFightingSwimmer     ; Function added by Vlad to let the Swimmer have multiple sprites
-	farcall IsFightingGuitarist   ; Function added by G-Dubs to let the Guitarist have multiple sprites
-    farcall IsFightingPokemaniac  ; Function added by G-Dubs to let the Pokemaniac have multiple sprites
-	farcall IsFightingBeauty      ; Function added by G-Dubs to let the Beauty have multiple sprites
-   ;farcall IsFightingBrock       ; Function added by G-Dubs to let the Gym Leaders have multiple sprites (for rematches)
-   ;farcall IsFightingMisty
-   ;farcall IsFightingLtSurge
-   ;farcall IsFightingErika
-   ;farcall IsFightingSabrina
-   ;farcall IsFightingKoga
-   ;farcall IsFightingJanine
-   ;farcall IsFightingBlaine	
-   ;farcall IsFightingGiovanni
-   ;farcall IsFightingLorelei
-   ;farcall IsFightingBruno
-   ;farcall IsFightingAgatha
-   ;farcall IsFightingLance
+	
+	; Call our new jump table-based handler for trainer sprites
+	farcall HandleAlternateTrainerSprite  ; Using farcall since it's in a different bank (ROMX)
 	jp BankswitchBack
 .linkBattle
 	ld hl, wTrainerPicPointer
@@ -48,24 +33,6 @@ GetTrainerInformation::
 	inc hl
 	ld [hl], d
 	ret
-
-;IsFightingJessieJames::
-;	ld a, [wTrainerClass]
-;	cp ROCKET
-;	ret nz
-;	ld a, [wTrainerNo]
-;	cp $2a
-;	ret c
-;	ld de, JessieJamesPic
-;	cp $2e
-;	jr c, .dummy
-;	ld de, JessieJamesPic ; possibly meant to add another pic
-;.dummy
-;	ld hl, wTrainerPicPointer
-;	ld a, e
-;	ld [hli], a
-;	ld [hl], d
-;	ret
 
 GetTrainerName::
 	farjp GetTrainerName_
