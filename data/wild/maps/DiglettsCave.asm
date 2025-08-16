344648fb6dfb6f1dadef0582f6f0335a325dfa7a
G-Dubs <wie15003@byui.edu>
2025-08-16 19:18:28 -0400

    Changed a couple of tiles in Diglett's Cave
    Changed a single tile on Route 2
    
    Modified the wild encounter rate in Diglett's Cave

--- data/wild/maps/DiglettsCave.asm
+++ data/wild/maps/DiglettsCave.asm
@@ -1,16 +1,21 @@
 DiglettsCaveWildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 18, DIGLETT
-	db 19, DIGLETT
-	db 17, DIGLETT
-	db 20, DIGLETT
-	db 16, DIGLETT
-	db 15, DIGLETT
-	db 21, DIGLETT
-	db 22, DIGLETT
-	db 29, DUGTRIO
-	db 31, DUGTRIO
+	db 21, DIGLETT        ; 20%
+	db 20, DIGLETT        ; 20%
+	db 19, DIGLETT        ; 15%
+	db 22, DIGLETT        ; 10%
+	db 18, DIGLETT        ; 10%
+	db 17, ZUBAT          ; 10%
+	db 23, DIGLETT        ;  5%
+	db 29, DUGTRIO        ;  5%
+	db 31, DUGTRIO        ;  4%
+	db 19, ONIX           ;  1%
 	end_grass_wildmons
+
+    ; Diglett  80%
+	; Zubat    10%
+	; Dugtrio   9%
+	; Onix      1%
 
 	def_water_wildmons 0 ; encounter rate
 	end_water_wildmons
