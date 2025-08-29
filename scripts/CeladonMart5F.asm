0ccbeda55759ca0ec80200d34f678b075e2a58b7
G-Dubs <wie15003@byui.edu>
2025-08-29 19:56:45 -0400

    Made a chart to have different inventories in the various PokéMarts

--- scripts/CeladonMart5F.asm
+++ scripts/CeladonMart5F.asm
@@ -23,7 +23,53 @@ CeladonMart5FCurrentFloorSignText:
 	text_end
 
 CeladonMart5FClerk1Text:		                                     ; New text added by G-Dubs (Followed tutorial to free up space in the home bank)
+	ld hl, wObtainedBadges                                           ; New functions added by G-Dubs to test badge specific mart inventories
+	; Check for Earth Badge (8th)
+	bit BIT_EARTHBADGE, [hl]
+	jr nz, .EarthStock
+
+    ; Check for Marsh Badge (6th)
+	bit BIT_MARSHBADGE, [hl]
+	jr nz, .MarshStock
+
+	; Check for Rainbow Badge (4th)
+	bit BIT_RAINBOWBADGE, [hl]
+	jr nz, .RainbowStock
+
+	; Check for Cascade Badge (2nd)
+	bit BIT_CASCADEBADGE, [hl]
+	jr nz, .CascadeStock
+
+; Default 
+	; Inventory for 0-1 Badges
 	script_mart X_ACCURACY, GUARD_SPEC, DIRE_HIT, X_ATTACK, X_DEFEND, X_SPEED, X_SPECIAL
 
+.CascadeStock
+	; Inventory for 2-3 Badges
+	script_mart X_ACCURACY, GUARD_SPEC, DIRE_HIT, X_ATTACK, X_DEFEND, X_SPEED, X_SPECIAL, ETHER
+
+.RainbowStock
+	; Inventory for 4-5 Badges
+	script_mart X_ACCURACY, GUARD_SPEC, DIRE_HIT, X_ATTACK, X_DEFEND, X_SPEED, X_SPECIAL, ETHER, ELIXIR
+
+.MarshStock
+	; Inventory for 6-7 Badges
+	script_mart X_ACCURACY, GUARD_SPEC, DIRE_HIT, X_ATTACK, X_DEFEND, X_SPEED, X_SPECIAL, ETHER, ELIXIR, MAX_ETHER
+
+.EarthStock
+    ; Inventory for 8 (All) Badges
+    script_mart X_ACCURACY, GUARD_SPEC, DIRE_HIT, X_ATTACK, X_DEFEND, X_SPEED, X_SPECIAL, ETHER, ELIXIR, MAX_ETHER, MAX_ELIXIR
+	
 CeladonMart5FClerk2Text:		                                     ; New text added by G-Dubs (Followed tutorial to free up space in the home bank)
+	ld hl, wObtainedBadges                                           ; New functions added by G-Dubs to test badge specific mart inventories
+	; Check for Earth Badge (8th)
+	bit BIT_EARTHBADGE, [hl]
+	jr nz, .EarthStock
+
+; Default 
+	; Inventory for 0-7 Badges
 	script_mart HP_UP, PROTEIN, IRON, CARBOS, CALCIUM
+
+.EarthStock
+    ; Inventory for 8 (All) Badges
+    script_mart HP_UP, PROTEIN, IRON, CARBOS, CALCIUM, PP_UP
