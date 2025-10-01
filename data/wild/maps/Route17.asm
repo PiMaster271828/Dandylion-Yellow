2eb167e94b9b16a3bddbc2682d7d09ef2d8beddb
G-Dubs <wie15003@byui.edu>
2025-10-01 15:49:56 -0400

    Fixed water encounters for Route 17 &  18

--- data/wild/maps/Route17.asm
+++ data/wild/maps/Route17.asm
@@ -26,18 +26,18 @@ Route17WildMons:
 	; Rapidash    1%
 
 	def_water_wildmons 3 ; encounter rate
-	db 20, TENTACOOL      ; 15%
-	db 25, TENTACOOL      ; 15%
-	db 30, TENTACOOL      ; 15%
-	db 35, TENTACOOL      ; 15%
-	db  5, TENTACOOL      ; 10%
-	db 10, TENTACOOL      ; 10%
-	db 15, TENTACOOL      ; 10%
-	db 25, SHELLDER       ;  5%
-	db 30, SHELLDER       ;  2%
-	db 35, CLOYSTER       ;  1%
-	db 40, CLOYSTER       ;  1%
-	db 40, TENTACOOL      ;  1%
+	db 20, TENTACOOL     ; 15%
+	db 25, TENTACOOL     ; 15%
+	db 30, TENTACOOL     ; 15%
+	db 35, TENTACOOL     ; 15%
+	db  5, TENTACOOL     ; 10%
+	db 10, TENTACOOL     ; 10%
+	db 15, TENTACOOL     ; 10%
+	db 25, SHELLDER      ;  5%
+	db 30, SHELLDER      ;  2%
+	db 35, CLOYSTER      ;  1%
+	db 40, CLOYSTER      ;  1%
+	db 40, TENTACOOL     ;  1%
 	end_water_wildmons
     
 	; Tentacool   91%
--- data/wild/maps/Route18.asm
+++ data/wild/maps/Route18.asm
@@ -24,5 +24,21 @@ Route18WildMons:
 	; Pidgeotto   1%
 	; Pidgeot     1%
 
-	def_water_wildmons 0 ; encounter rate
+	def_water_wildmons 3 ; encounter rate
+	db 20, TENTACOOL     ; 15%
+	db 25, TENTACOOL     ; 15%
+	db 30, TENTACOOL     ; 15%
+	db 35, TENTACOOL     ; 15%
+	db  5, TENTACOOL     ; 10%
+	db 10, TENTACOOL     ; 10%
+	db 15, TENTACOOL     ; 10%
+	db 25, SHELLDER      ;  5%
+	db 30, SHELLDER      ;  2%
+	db 35, CLOYSTER      ;  1%
+	db 40, CLOYSTER      ;  1%
+	db 40, TENTACOOL     ;  1%
 	end_water_wildmons
+    
+	; Tentacool   91%
+	; Shelder      7%
+	; Cloyster     2%
