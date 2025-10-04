e28818fe2728045ae920b46e33ee5cd7fcf02754
G-Dubs <wie15003@byui.edu>
2025-10-04 15:23:07 -0400

    Moved comments from Route 1 to Route 2

--- text/Route2.asm
+++ text/Route2.asm
@@ -25,3 +25,41 @@ _Route2AltSignSouthText::         ; New sign added by G-Dubs
 	line "DIGLETT's CAVE -"
 	cont "VIRIDIAN CITY"
 	done
+
+/*
+Trainer 02:
+    text "Are you a #MON"
+	line "trainer? 
+	
+	para "Let's battle!"
+	done
+
+    text "Ah"
+	line "Man. I won my"
+	cont "last battle."
+    prompt
+
+    text "I hear being a"
+	line "good trainer is"
+	cont "really hard. I'm"
+	cont "going to battle"
+	cont "other trainers to"
+	cont "get stronger."
+    done   
+
+Trainer 03:
+    text "There aren't any"
+	line "bug #MON here,"
+	cont "but I do see a"
+	cont "#MON trainer!"
+    done 
+	
+	text "Argh!"
+	line "You're strong!"
+    prompt
+
+	text "I ran out of"
+	line "#BALLS. I need to"
+	cont "go buy some more."
+	done
+*/
