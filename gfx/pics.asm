4f55cd598c3aa267893a2c931ad6dc06361ab4a7
G-Dubs <wie15003@byui.edu>
2025-10-10 14:51:04 -0400

    Added Abra back sprite
    Added Koffing, Dragonair, & Dragonite alt. sprites

--- gfx/pics.asm
+++ gfx/pics.asm
@@ -104,7 +104,7 @@ ElectabuzzPicFront::  INCBIN "gfx/pokemon/front/electabuzz.pic"
 ElectabuzzPicBack::   INCBIN "gfx/pokemon/back/electabuzzb.pic"
 MagnetonPicFront::    INCBIN "gfx/pokemon/front/magneton.pic"              
 MagnetonPicBack::     INCBIN "gfx/pokemon/back/magnetonb.pic"
-KoffingPicFront::     INCBIN "gfx/pokemon/front/koffingredandblueedited.pic"
+KoffingPicFront::     INCBIN "gfx/pokemon/front/koffinggreengeorge.pic"        ; Default Koffing sprite changed by G-Dubs
 KoffingPicBack::      INCBIN "gfx/pokemon/back/koffingb.pic"
 MankeyPicFront::      INCBIN "gfx/pokemon/front/mankey.pic"
 MankeyPicBack::       INCBIN "gfx/pokemon/back/mankeyb.pic"
@@ -255,7 +255,7 @@ MarowakPicBack::       INCBIN "gfx/pokemon/back/marowakb.pic"
 HaunterPicFront::      INCBIN "gfx/pokemon/front/hauntergeorge.pic"            ; Haunter default sprite modified by G-Dubs
 HaunterPicBack::       INCBIN "gfx/pokemon/back/haunterb.pic"
 AbraPicFront::         INCBIN "gfx/pokemon/front/abra.pic"
-AbraPicBack::          INCBIN "gfx/pokemon/back/abrab.pic"
+AbraPicBack::          INCBIN "gfx/pokemon/back/abranewb.pic"
 AlakazamPicFront::     INCBIN "gfx/pokemon/front/alakazam.pic"
 AlakazamPicBack::      INCBIN "gfx/pokemon/back/alakazamb.pic"
 PidgeottoPicFront::    INCBIN "gfx/pokemon/front/pidgeotto.pic"
@@ -328,7 +328,7 @@ SECTION "Alt Pokemon Sprites 1", ROMX                                          ;
 
 RocketZubatPicFront::        INCBIN "gfx/pokemon/front/zubat.pic"              ; Rocket Grunt's PokéMon sprite added by G-Dubs
 ;RocketGolbatPicFront::       INCBIN "gfx/pokemon/front/golbat.pic"             
-RocketKoffingPicFront::      INCBIN "gfx/pokemon/front/koffingsilver.pic"
+RocketKoffingPicFront::      INCBIN "gfx/pokemon/front/koffingredandblueedited.pic"
 RocketGrimerPicFront::       INCBIN "gfx/pokemon/front/grimersilver.pic"
 RocketEkansPicFront::        INCBIN "gfx/pokemon/front/ekansblackwhite.pic"
 RocketRattataPicFront::      INCBIN "gfx/pokemon/front/rattatasilver.pic"        
