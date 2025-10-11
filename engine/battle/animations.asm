9cc9688a5b0416f48baa90a47ba4322e6c2c52c7
G-Dubs <wie15003@byui.edu>
2025-10-11 17:29:01 -0400

    Fixed a bug (according to the tutorial) where the Strength smoke particles did not show properly

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -1413,9 +1413,9 @@ AdjustOAMBlockYPos2:
 	add b
 	cp 112
 	jr c, .skipSettingPreviousEntrysAttribute
-	dec hl
+   ;dec hl
 	ld a, 160 ; bug, sets previous OAM entry's attribute
-	ld [hli], a
+   ;ld [hli], a
 .skipSettingPreviousEntrysAttribute
 	ld [hl], a
 	add hl, de
